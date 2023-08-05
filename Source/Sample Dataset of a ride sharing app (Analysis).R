1085b3fa5afafa7a228a647e90e28a54aea2110d
DESKTOP-D53F1L2\HP <sanjidatahsin174@gmail.com>
2023-08-05 18:12:26 +0600

    Convert the rating, payment_type, canceled_by, rider_id, driver_id into character from integers and run the colSums command again.

--- Source/Sample Dataset of a ride sharing app (Analysis).R
+++ Source/Sample Dataset of a ride sharing app (Analysis).R
@@ -1,7 +1,14 @@
 Dataset_1<- read.csv("D:\\Projects\\Dataset\\Sample Dataset of a ride sharing app (Data).csv", header=TRUE)
 View(Dataset_1)
+
+Dataset_1$rating <- as.character(Dataset_1$rating)
+Dataset_1$payment_type <- as.character(Dataset_1$payment_type)
+Dataset_1$canceled_by <- as.character(Dataset_1$canceled_by)
+Dataset_1$rider_id <- as.character(Dataset_1$rider_id)
+Dataset_1$driver_id <- as.character(Dataset_1$driver_id)
+
 colSums(Dataset_1 != 0)
-#hashed_id                rider_id               driver_id                    fare 
+#      hashed_id                rider_id               driver_id                    fare 
 #5085                    5085                    3682                    2394 
 #estimated_fare           ride_duration estimated_ride_duration           ride_distance 
 #5085                      NA                    5085                      NA 
@@ -9,5 +16,7 @@ colSums(Dataset_1 != 0)
 #5070                    5085                      NA                    5085 
 #undiscounted_fare        review_requested                discount             canceled_by 
 #NA                      24                      NA                      NA 
-#payment_type                       X                     X.1 
-#5085                      NA                      NA 
+#payment_type 
+#5085 
+
+freq(Dataset_1)
