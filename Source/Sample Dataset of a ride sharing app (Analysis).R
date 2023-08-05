59d8dcaa1e907b6c401ae2db89622634b479d491
DESKTOP-D53F1L2\HP <sanjidatahsin174@gmail.com>
2023-08-06 00:46:30 +0600

    Final code to answer what are the factors that are causing ride cancelation, and to test if the customers are facing overcharges.

--- Source/Sample Dataset of a ride sharing app (Analysis).R
+++ Source/Sample Dataset of a ride sharing app (Analysis).R
@@ -60,3 +60,65 @@ plot(density(Dataset_1$estimated_fare), pch = 19, lwd = 3, main = "Estimated_far
 plot(density(Dataset_1$estimated_ride_distance), pch = 19, lwd = 3, main = "ride_distance")
 
 # clearly these variables are normally distributed. the graphs obtained are shared.
+
+
+plot(Dataset_1$estimated_ride_distance, Dataset_1$estimated_fare)
+plot(Dataset_1$estimated_ride_duration, Dataset_1$estimated_fare)
+
+# There is a linear relation indicates that the fare is dependent on ride distance and ride duration. 
+# We can also determine correlation coefficients to understand which variables effect the fare most.But first lets add
+# some new columns like
+#	First of all, we recoded the status variable (1=COMPLETED, 2= CANCELED) and named the variable status_rec.
+#	Then we analyzed the driver_id variable and found that 1403 cells are empty so we added a new variable 
+#    driver_assigned (1 = driver_id available, 0= driver_id cell is empty).
+#	Analyzing the variable canceled_by, we added another variable canceled_by_type (0= trip was completed, 
+#    1= rider_id, 2=driver_id, 3=system cancel). And read the dataset again.
+
+Correlation_matrix<- data.frame(cor(Dataset_1[,c("fare", "estimated_fare", "estimated_ride_duration",
+                  "estimated_ride_distance","canceled_by_type", "driver_assigned", "status_rec")]))
+view(Correlation_matrix)
+
+
+#                        fare estimated_fare estimated_ride_duration estimated_ride_distance canceled_by_type
+#fare                     1.0000000     0.29125377            0.3337597602              0.34430360    -0.6680798732
+#estimated_fare           0.2912538     1.00000000            0.8795054423              0.91350197     0.0879046014
+#estimated_ride_duration  0.3337598     0.87950544            1.0000000000              0.95162120    -0.0003479046
+#estimated_ride_distance  0.3443036     0.91350197            0.9516211963              1.00000000    -0.0127741359
+#canceled_by_type        -0.6680799     0.08790460           -0.0003479046             -0.01277414     1.0000000000
+#driver_assigned          0.1760921    -0.13975509           -0.1174742051             -0.11385952    -0.2697112166
+#status_rec              -0.8245116     0.03943109           -0.0476437019             -0.04458027     0.8102734795
+#driver_assigned  status_rec
+#fare                          0.1760921 -0.82451159
+#estimated_fare               -0.1397551  0.03943109
+#estimated_ride_duration      -0.1174742 -0.04764370
+#estimated_ride_distance      -0.1138595 -0.04458027
+#canceled_by_type             -0.2697112  0.81027348
+#driver_assigned               1.0000000 -0.21357136
+#status_rec                   -0.2135714  1.00000000
+
+# correlation_matrix is also added
+#From the correlation matrix, it is clear that the status of a trip has a strong positive correlation with 
+#canceled_by_type and a strong negative correlation with fare. The correlation between status_rec and 
+#driver_assigned is also negative but moderate. So these three are the important factors in the case of 
+#reliability. Also, canceled_by_type has a strong negative relationship with driver_assigned.
+
+# Finally we can conduct a t test to compare the fare and estimated fare.
+
+summary(Dataset_1$estimated_fare)
+#Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#30.0    90.0   121.0   126.3   154.0   665.0 
+
+ t.test(Dataset_1$fare, mu = 126.3, alternative = "less")
+
+#One Sample t-test
+
+#data:  Dataset_1$fare
+#t = -96.454, df = 5084, p-value < 2.2e-16
+#alternative hypothesis: true mean is less than 126.3
+#95 percent confidence interval:
+#  -Inf 47.47169
+#sample estimates:
+#  mean of x 
+#46.10383 
+
+# Overall, the analysis suggests that users are not facing significant overcharges.
